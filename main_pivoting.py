ff21307247353e295760f1cd728989bdacb84ea6
apat20 <apat201094@gmail.com>
2025-03-12 14:01:35 -0400

    separate class for grasping created

--- main_pivoting.py
+++ main_pivoting.py
@@ -13,6 +13,9 @@ from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 # Functionalities for point cloud processing and computing the ideal grasping region:
 from point_cloud_module.process_point_cloud import point_cloud
 
+# Functionalities for computing grasping regions and corresponding 6DOF end-effector poses:
+from point_cloud_module.tograsp import tograsp
+
 from time import perf_counter
 import argparse
 
@@ -69,15 +72,15 @@ def build_cloud_object(cloud_object, pcd):
     return cloud_object
 
 '''Function to compute the final grasp pose after the pivoting motion'''
-def pivoting_motion(cloud_object, num_pose):
+def pivoting_motion(cloud_object, grasp_object, num_pose):
     # Computing the final end-effector pose after grasping based on the screw axis:
     pitch = 0
     theta = math.radians(90)
-    g = get_transformation_for_screw(cloud_object.unit_vector, pitch, theta, cloud_object.point)
+    g = get_transformation_for_screw(grasp_object.unit_vector, pitch, theta, grasp_object.point)
     
     # desired_grasp_pose_object_frame = cloud_object.computed_end_effector_poses[num_pose]
     # desired_grasp_pose_object_frame = cloud_object.approach_dir_2_poses[num_pose]
-    desired_grasp_pose_object_frame = cloud_object.approach_dir_other_poses[num_pose]
+    desired_grasp_pose_object_frame = grasp_object.approach_dir_other_poses[num_pose]
     
     # desired_grasp_pose_object_frame = cloud_object.computgrasping_poses, grasp_flaged_end_effector_poses[num_pose]
     desired_grasp_pose_object_frame_final = np.matmul(g, desired_grasp_pose_object_frame)
@@ -87,7 +90,7 @@ def pivoting_motion(cloud_object, num_pose):
 
     # Storing the poses for preprocessing: 
     # grasping_poses = np.asarray([cloud_object.computed_end_effector_poses_inter_base[num_pose], cloud_object.computed_end_effector_poses_base[num_pose], desired_grasp_pose_base_frame_final])
-    grasping_poses = np.asarray([cloud_object.approach_dir_other_inter_poses_base[num_pose], cloud_object.approach_dir_other_poses_base[num_pose], desired_grasp_pose_base_frame_final])
+    grasping_poses = np.asarray([grasp_object.approach_dir_other_inter_poses_base[num_pose], grasp_object.approach_dir_other_poses_base[num_pose], desired_grasp_pose_base_frame_final])
     # grasping_poses = np.asarray([cloud_object.approach_dir_2_inter_poses_base[num_pose], cloud_object.approach_dir_2_poses_base[num_pose], desired_grasp_pose_base_frame_final])
     
     grasping_poses =np.reshape(grasping_poses, [len(grasping_poses)*4,4])
@@ -97,7 +100,7 @@ def pivoting_motion(cloud_object, num_pose):
     return desired_grasp_pose_base_frame_final, grasping_poses, grasp_flag
 
 '''Function to visualize the results: '''
-def visualize(cloud_object, num_pose, desired_grasp_pose_base_frame_final):
+def visualize(cloud_object, grasp_object, num_pose, desired_grasp_pose_base_frame_final):
     # Extracting the points for visualization purposes:
     x_points = np.reshape(cloud_object.points[:, 0], [cloud_object.points.shape[0],1])
     y_points = np.reshape(cloud_object.points[:, 1], [cloud_object.points.shape[0],1])
@@ -111,21 +114,21 @@ def visualize(cloud_object, num_pose, desired_grasp_pose_base_frame_final):
     fig1 = plt.figure()
     ax1 = fig1.add_subplot(projection='3d')
     ax1.grid(False)
-    cloud_object.vertices = cloud_object.transformed_vertices_object_frame
-    cloud_object.plot_cube()
-    ax1.add_collection3d(Poly3DCollection(cloud_object.faces, linewidths=1, edgecolors='b', alpha=.25))
+    grasp_object.vertices = cloud_object.transformed_vertices_object_frame
+    grasp_object.plot_cube()
+    ax1.add_collection3d(Poly3DCollection(grasp_object.faces, linewidths=1, edgecolors='b', alpha=.25))
     ax1.scatter(x_transformed_points, y_transformed_points, z_transformed_points, s = 0.2)
 
     # Visualize the screw axis: 
-    ax1.scatter(cloud_object.point[0], cloud_object.point[1], cloud_object.point[2], marker = '*', s = 100, color = 'r')
-    ax1.quiver(cloud_object.point[0], cloud_object.point[1], cloud_object.point[2], 0.25*cloud_object.unit_vector[0], 0.25*cloud_object.unit_vector[1], 0.25*cloud_object.unit_vector[2], color = "r", arrow_length_ratio = 0.25)
+    ax1.scatter(grasp_object.point[0], grasp_object.point[1], grasp_object.point[2], marker = '*', s = 100, color = 'r')
+    ax1.quiver(grasp_object.point[0], grasp_object.point[1], grasp_object.point[2], 0.25*grasp_object.unit_vector[0], 0.25*grasp_object.unit_vector[1], 0.25*grasp_object.unit_vector[2], color = "r", arrow_length_ratio = 0.25)
 
     # Base reference Frame: 
-    cloud_object.R = cloud_object.R_base
-    cloud_object.p = cloud_object.p_base
-    cloud_object.scale_value = 0.45
-    cloud_object.length_value = 0.25
-    ax1 = cloud_object.plot_reference_frames(ax1)
+    grasp_object.R = cloud_object.R_base
+    grasp_object.p = cloud_object.p_base
+    grasp_object.scale_value = 0.45
+    grasp_object.length_value = 0.25
+    ax1 = grasp_object.plot_reference_frames(ax1)
 
     ax1.set_xlabel('X')
     ax1.set_ylabel('Y')
@@ -139,19 +142,19 @@ def visualize(cloud_object, num_pose, desired_grasp_pose_base_frame_final):
     fig2 = plt.figure()
     ax2 = fig2.add_subplot(projection='3d')
     ax2.scatter(x_transformed_points, y_transformed_points, z_transformed_points, s = 0.2)
-    ax2.scatter(cloud_object.point[0], cloud_object.point[1], cloud_object.point[2], marker = '*', s = 100, color = 'r')
-    ax2.quiver(cloud_object.point[0], cloud_object.point[1], cloud_object.point[2], 0.25*cloud_object.unit_vector[0], 0.25*cloud_object.unit_vector[1], 0.25*cloud_object.unit_vector[2], color = "r", arrow_length_ratio = 0.25)
+    ax2.scatter(grasp_object.point[0], grasp_object.point[1], grasp_object.point[2], marker = '*', s = 100, color = 'r')
+    ax2.quiver(grasp_object.point[0], grasp_object.point[1], grasp_object.point[2], 0.25*grasp_object.unit_vector[0], 0.25*grasp_object.unit_vector[1], 0.25*grasp_object.unit_vector[2], color = "r", arrow_length_ratio = 0.25)
     
     # Base reference Frame: 
-    cloud_object.R = cloud_object.R_base
-    cloud_object.p = cloud_object.p_base
-    cloud_object.scale_value = 0.45
-    cloud_object.length_value = 0.25
-    ax2 = cloud_object.plot_reference_frames(ax2)
+    grasp_object.R = cloud_object.R_base
+    grasp_object.p = cloud_object.p_base
+    grasp_object.scale_value = 0.45
+    grasp_object.length_value = 0.25
+    ax2 = grasp_object.plot_reference_frames(ax2)
 
     # Grasp centers:
-    for i,v in enumerate(cloud_object.computed_end_effector_poses_base):
-        g = cloud_object.grasp_centers[i]
+    for i,v in enumerate(grasp_object.computed_end_effector_poses_base):
+        g = grasp_object.grasp_centers[i]
         ax2.scatter(g[0], g[1], g[2], marker = '*', s = 20, color = 'r')
 
     ax2.set_xlabel('X')
@@ -169,49 +172,49 @@ def visualize(cloud_object, num_pose, desired_grasp_pose_base_frame_final):
     ax3.scatter(x_points, y_points, z_points, s = 0.2)
     
     # Visualize the screw axis: 
-    ax3.scatter(cloud_object.point_base[0], cloud_object.point_base[1], cloud_object.point_base[2], marker = '*', s = 100, color = 'r')
-    ax3.quiver(cloud_object.point_base[0], cloud_object.point_base[1], cloud_object.point_base[2], 0.25*cloud_object.unit_vector_base[0], 0.25*cloud_object.unit_vector_base[1], 0.25*cloud_object.unit_vector_base[2], color = "r", arrow_length_ratio = 0.25)
+    ax3.scatter(grasp_object.point_base[0], grasp_object.point_base[1], grasp_object.point_base[2], marker = '*', s = 100, color = 'r')
+    ax3.quiver(grasp_object.point_base[0], grasp_object.point_base[1], grasp_object.point_base[2], 0.25*grasp_object.unit_vector_base[0], 0.25*grasp_object.unit_vector_base[1], 0.25*grasp_object.unit_vector_base[2], color = "r", arrow_length_ratio = 0.25)
 
     # Base reference Frame:
-    cloud_object.R = cloud_object.R_base
-    cloud_object.p = cloud_object.p_base
-    cloud_object.scale_value = 0.25
-    cloud_object.length_value = 0.15
-    ax3 = cloud_object.plot_reference_frames(ax3)
+    grasp_object.R = cloud_object.R_base
+    grasp_object.p = cloud_object.p_base
+    grasp_object.scale_value = 0.25
+    grasp_object.length_value = 0.15
+    ax3 = grasp_object.plot_reference_frames(ax3)
 
     # Bounding box reference frame:
-    cloud_object.R = cloud_object.R_bounding_box
-    cloud_object.p = cloud_object.p_bounding_box
-    cloud_object.scale_value = 0.15
-    cloud_object.length_value = 0.15
-    ax3 = cloud_object.plot_reference_frames(ax3)
+    grasp_object.R = cloud_object.R_bounding_box
+    grasp_object.p = cloud_object.p_bounding_box
+    grasp_object.scale_value = 0.15
+    grasp_object.length_value = 0.15
+    ax3 = grasp_object.plot_reference_frames(ax3)
 
     # Pregrasp pose:
     # pose = cloud_object.computed_end_effector_poses_inter_base[num_pose]
-    pose = cloud_object.approach_dir_other_inter_poses_base[num_pose]
+    pose = grasp_object.approach_dir_other_inter_poses_base[num_pose]
     # pose = cloud_object.approach_dir_2_inter_poses_base[num_pose]
-    cloud_object.R = pose[0:3, 0:3]
-    cloud_object.p = pose[0:3, 3]
-    cloud_object.scale_value = 0.025
-    cloud_object.length_value = 0.025
-    ax3 = cloud_object.plot_reference_frames(ax3)
+    grasp_object.R = pose[0:3, 0:3]
+    grasp_object.p = pose[0:3, 3]
+    grasp_object.scale_value = 0.025
+    grasp_object.length_value = 0.025
+    ax3 = grasp_object.plot_reference_frames(ax3)
 
     # Grasp pose:
     # pose = cloud_object.computed_end_effector_poses_base[num_pose]
-    pose = cloud_object.approach_dir_other_poses_base[num_pose]
+    pose = grasp_object.approach_dir_other_poses_base[num_pose]
     # pose = cloud_object.approach_dir_2_poses_base[num_pose]
-    cloud_object.R = pose[0:3, 0:3]
-    cloud_object.p = pose[0:3, 3]
-    cloud_object.scale_value = 0.025
-    cloud_object.length_value = 0.025
-    ax3 = cloud_object.plot_reference_frames(ax3)
+    grasp_object.R = pose[0:3, 0:3]
+    grasp_object.p = pose[0:3, 3]
+    grasp_object.scale_value = 0.025
+    grasp_object.length_value = 0.025
+    ax3 = grasp_object.plot_reference_frames(ax3)
 
     # Pose after pivoting:
-    cloud_object.R = desired_grasp_pose_base_frame_final[0:3, 0:3]
-    cloud_object.p = desired_grasp_pose_base_frame_final[0:3, 3]
-    cloud_object.scale_value = 0.025
-    cloud_object.length_value = 0.025
-    ax3 = cloud_object.plot_reference_frames(ax3)
+    grasp_object.R = desired_grasp_pose_base_frame_final[0:3, 0:3]
+    grasp_object.p = desired_grasp_pose_base_frame_final[0:3, 3]
+    grasp_object.scale_value = 0.025
+    grasp_object.length_value = 0.025
+    ax3 = grasp_object.plot_reference_frames(ax3)
 
     plt.show()
 
@@ -229,38 +232,42 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     # Creating the cloud object and loading the necessary file:
-    cloud_object = point_cloud()
+    cloud = point_cloud()
 
     # Read the point cloud data from the specified file    
     pcd = o3d.io.read_point_cloud(args.filename)
-    cloud_object = build_cloud_object(cloud_object, pcd)
-
-    # Specifying gripper tolerances:
-    cloud_object.gripper_width_tolerance = 0.08
-    cloud_object.gripper_height_tolerance = 0.041
-
-    # Attributes to compute the location of the reference frame at the flange for the grasp pose and pre-grasp pose
-    cloud_object.g_delta = 0.0625
-    cloud_object.g_delta_inter = 0.0925
+    cloud = build_cloud_object(cloud, pcd)
 
     # STARTING TOTAL TIME:
     total_time_start = perf_counter()
 
     # Computing the bounding boxes corresponding to the object point cloud: 
-    cloud_object.compute_bounding_box()
+    cloud.compute_bounding_box()
 
     print('Bounding box computed!')
 
     # APPROXIMATING THE TASK-DEPENDENT GRASP METRIC:
+
+    # Initialize an object of tograsp:
+    grasp = tograsp(cloud)
+
+    # Specifying gripper tolerances:
+    grasp.gripper_width_tolerance = 0.08
+    grasp.gripper_height_tolerance = 0.041
+
+    # Attributes to compute the location of the reference frame at the flange for the grasp pose and pre-grasp pose
+    grasp.g_delta = 0.0625
+    grasp.g_delta_inter = 0.0925
+
     # TASK: Pivoting a Cuboidal box:
     # Screw Parameters:
     # Axis 1:
     u = np.asarray([0, 1, 0, 0])
-    p = np.asarray([cloud_object.transformed_vertices_object_frame[1,0], np.divide((cloud_object.transformed_vertices_object_frame[1,1] + cloud_object.transformed_vertices_object_frame[7,1]),2), cloud_object.transformed_vertices_object_frame[1,2], 1])
-    cloud_object.unit_vector_base = cloud_object.g_bounding_box @ u.T
-    cloud_object.point_base = cloud_object.g_bounding_box @ p.T
+    p = np.asarray([cloud.transformed_vertices_object_frame[1,0], np.divide((cloud.transformed_vertices_object_frame[1,1] + cloud.transformed_vertices_object_frame[7,1]),2), cloud.transformed_vertices_object_frame[1,2], 1])
+    grasp.unit_vector_base = cloud.g_bounding_box @ u.T
+    grasp.point_base = cloud.g_bounding_box @ p.T
 
-    cloud_object.generate_contacts()
+    grasp.generate_contacts()
     
     print('Antipodal contact locations sampled from the surface of the bounding box!')
 
@@ -268,7 +275,7 @@ if __name__ == "__main__":
     metric_time_start = perf_counter()
     
     # cloud_object.predict_metric()
-    cloud_object.predict_metric_generic()
+    grasp.predict_metric_generic()
 
     print('Metric Value Predicted using Neural Network ... ')
 
@@ -276,10 +283,10 @@ if __name__ == "__main__":
     metric_time_end = perf_counter()
 
     # Computing the ideal grasping region:
-    cloud_object.get_ideal_grasping_region()
+    grasp.get_ideal_grasping_region()
 
     # Computing the end-effector poses based on the ideal grasping region:
-    cloud_object.get_end_effector_poses()
+    grasp.get_end_effector_poses()
 
     # END TOTAL TIME:
     total_time_end = perf_counter()
@@ -291,17 +298,17 @@ if __name__ == "__main__":
     print(f'Time required for computing the metric: {metric_time_end-metric_time_start} seconds')
 
     # Number of contact locations generated on the surface:
-    print("Number of contact locations generated on the bounding box: ", cloud_object.sampled_c1.shape[0])
+    print("Number of contact locations generated on the bounding box: ", grasp.sampled_c1.shape[0])
 
     # Computed end-effector poses:
-    print("Number of end-effector poses computed: ", len(cloud_object.computed_end_effector_poses_base))
+    print("Number of end-effector poses computed: ", len(grasp.computed_end_effector_poses_base))
 
     # Selecting a random pre-grasp and grasp pose:
     num_pose = 10
     # Computing the final grasp pose after pivoting the cuboidal object:
-    desired_grasp_pose_base_frame_final, grasping_poses, grasp_flag = pivoting_motion(cloud_object, num_pose)
+    desired_grasp_pose_base_frame_final, grasping_poses, grasp_flag = pivoting_motion(cloud, grasp, num_pose)
     
     if args.visualize:
-        visualize(cloud_object, num_pose, desired_grasp_pose_base_frame_final)
+        visualize(cloud, grasp, num_pose, desired_grasp_pose_base_frame_final)
 
     
